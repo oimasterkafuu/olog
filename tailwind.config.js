4d42924c980fd5b11da1255e16121f3b7dd8e6d8
oimasterkafuu <oimasterkafuu@zohomail.cn>
2025-11-03 23:34:13 +0800

    chore(deps): 初始化项目依赖与配置

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./lib/**/*.{ts,tsx}", "./pages/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
